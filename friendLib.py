0edb228f9b776ffadf4cf92d173e6091393dd1c6
shadowninja826 <sodachheng@gmail.com>
2016-10-09 12:32:49 -0500

    new friends

---
+++ friendLib.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  9 12:16:33 2016
+
+@author: ninja
+"""
+
+print('welcome to friends mablibs by soda')
+name1 = input("Enter a name: ")
+name2 = input("Enter another name: ")
+noun1 = input("Enter a noun (thing): ")
+place1 = input("Enter a place: ")
+#country1 = input("Enter a country: ")
+adjective1 = input("Enter a adjective: ")
+#adjective2 = input("Enter another adjective: ")
+emotion1 =  input("Enter a emotion: ")
+animal1 = input("Enter an animal: ")
+animal2 = input("Enter an animal: ")
+food1 = input("Enter a food: ")
+verb1 = input("Enter a verb: ")
+verb2 = input("Enter a verb: ")
+verb3 = input("Enter a verb: ")
+#number1 = input("Enter a number: ")
+#number2 = input("Enter another number: ")
+print ("Once upon a time there was a "+animal1+" named "+name1+". She was a "+adjective1+chr(32)+animal1+" that liked to "+verb1+chr(44)+chr(32)+verb2+", and "+verb3+" outside.")
+print ("One day, while she was sitting on her "+noun1+" her owner decided to moved to "+place1+" and she couldn't do the fun stuff any more and that made her "+emotion1+chr(46))
+print ("But in the "+place1+" , she met a "+animal2+" named "+name2+" and they became good friends. "+name2+" taught "+name1+" new tricks and they had a lot of fun and ate "+food1+" together.")
+print ("Even though "+name1+" couldn't "+verb1+chr(44)+chr(32)+verb2+", and "+verb3+" outside any more, she forgot all about that stuff because of all the fun she had with "+name2+(chr(46)))
