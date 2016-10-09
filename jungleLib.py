fafc577e4e1d7e3fd599a9bdcc9a2616d3344060
shadowninja826 <sodachheng@gmail.com>
2016-10-09 08:30:35 -0500

    added the python story

---
+++ jungleLib.py
@@ -0,0 +1,3 @@
+print("Once upon a time there was a man named "name1". He had a daughter named "name2".")
+print("And they lived in a "country1" jungle. One day, a "animal1" came and tried to "verb1" his daughter.")
+print("But he "verb2" the "animal1" up in pieces for dinner. "Name1" and "name2" ate the "animal1".")
