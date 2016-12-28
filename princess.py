4169b797711e10b0321b699350c3f102566f8d0f
shadowninja826 <sodachheng@gmail.com>
2016-12-28 11:23:02 -0600

    new princess story
    Original story by my tiger girl

---
+++ princess.py
@@ -0,0 +1,8 @@
+''' 
+Once upon a time there is a beautiful princess named princess Alice. 
+She lived in a far away castle in a beautiful Jungle with flowers tigers and tress with delicious fruit. 
+One day she decided to leave the castle and go on a trip and ran into a big dragon that breath fire. 
+And then she remembers that her was there and it was going to destroy it, but it was good thing she brought her sword and gun. 
+So she fought it and she chopped off one of his leg and saved the kingdom. 
+A few weeks later his body rotted and turned into a big mountain that they turned into a playground. The End
+'''
