dad4a985cabec865ee431b286b8b1975eecb87a0
shadowninja826 <sodachheng@gmail.com>
2016-12-29 11:38:59 -0600

    updated code version

--- princess.py
+++ princess.py
@@ -1,8 +1,31 @@
-''' 
-Once upon a time there is a beautiful princess named princess Alice. 
-She lived in a far away castle in a beautiful Jungle with flowers tigers and tress with delicious fruit. 
-One day she decided to leave the castle and go on a trip and ran into a big dragon that breath fire. 
-And then she remembers that her was there and it was going to destroy it, but it was good thing she brought her sword and gun. 
-So she fought it and she chopped off one of his leg and saved the kingdom. 
-A few weeks later his body rotted and turned into a big mountain that they turned into a playground. The End
-'''
+#Python 2.7
+
+'''Once upon a time there is a beautiful princess named princess Alice.
+She lived in a far away castle in a beautiful Jungle with flowers tigers and tress with delicious fruit.
+One day she decided to leave the castle and go on a trip and ran into a big dragon that breath fire.
+And then she remembers that her was there and it was going to destroy it, but it was good thing she brought her sword and gun.
+So she fought it and she chopped off one of his leg and saved the kingdom.
+A few weeks later his body rotted and turned into a big mountain that they turned into a playground. The End'''
+
+adj1 = raw_input("Enter a adjective: ")
+name1 = raw_input("Enter a name: ")
+place1 = raw_input("Enter a place: ")
+noun1 = raw_input("Enter a noun: ")
+noun2 = raw_input("Enter a noun: ")
+noun3 = raw_input("Enter a noun: ")
+food1 = raw_input("Enter a food: ")
+noun4 = raw_input("Enter a noun: ")
+place2 = raw_input("Enter a place: ")
+animal1 = raw_input("Enter a animal")
+noun5 = raw_input("Enter a noun: ")
+noun6 = raw_input("Enter a noun: ")
+bodypart1 = raw_input("Enter a body part: ")
+adj2 = raw_input("Enter a adjective: ")
+
+
+print "Once upon a time there is a " + adj1 + " princess named princess " +name1+"."
+print "She lived in a far away castle in a beautiful "+place1 +" with "+noun1+" " +noun2+ " and "+noun3 +" with delicious "+food1+"."
+print "One day she decided to leave the "+place2+ " and go on a trip and ran into a big "+animal1+" that breath "+noun4+"."
+print "And then she remembers that her "+place2+ " was there and "+animal1+" was going to destroy it, but it was good thing she brought her "+noun5+" and "+noun6+"."
+print "So she fought it and she chopped off one of his " +bodypart1+" and saved the kingdom."
+print "A few weeks later "+animal1+" body rotted and turned into a big mountain that they turned into a "+adj2+" playground. The End"
